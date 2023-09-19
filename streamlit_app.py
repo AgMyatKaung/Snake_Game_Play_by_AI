1ebc448a4bc6d4eb82cec7578955e882a428f9a1
Aung Myat Kaung <107551174+AgMyatKaung@users.noreply.github.com>
2023-09-19 14:37:42 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ from game import SnakeGameAI, Direction, Point
 from agent import Agent  # Import your Agent class from agent.py
 import matplotlib.pyplot as plt
 import time
-import subprocess  # Import subprocess module
 
 # Function to run the game loop
 def run_game(agent, game):
+
+
     scores = []
     highest_score = 0
 
@@ -35,53 +36,27 @@ def run_game(agent, game):
             # Update the live chart
             chart.line_chart(scores)
 
-            st.write(f"Game {agent.n_games}: Score {score}, Highest Score: {highest_score}")
+            #st.write(f"Game {agent.n_games}: Score {score}, Highest Score: {highest_score}")
 
         # Add a sleep to control the speed of the game (adjust as needed)
         time.sleep(0.01)  # Set a very low sleep duration to speed up the game
 
-# Set page title and description
-st.set_page_config(page_title="Snake Game AI", page_icon="🐍")
+# Create buttons for training and stopping the game
 st.title("Snake Game AI")
+st.text("Press 'Ctrl+C' to stop the game.")
+train_btn = st.button("Train AI")
+stop_btn = st.button("Stop")
 
-# Create a two-column layout
-col1, col2 = st.columns([2, 1])
+if train_btn:
+    st.text("AI training in progress...")
 
-# Column 1: Snake Game AI
-with col1:
-    st.subheader("Game Display")
-    
     # Create or load the agent and game instances
     agent = Agent()
     agent.load_model()
     game = SnakeGameAI()
 
-    # Display game controls
-    st.markdown("## Game Controls")
-    st.text("Press 'Ctrl+C' to stop the game.")
+    # Run the game loop
+    run_game(agent, game)
 
-    # Check if "Train AI" button is clicked
-    if st.button("Train AI"):
-        st.text("AI training in progress...")
-        
-        # Use subprocess to run the game in the console
-        subprocess.Popen(["python", "your_game_script.py"])
-
-# Column 2: Buttons
-with col2:
-    st.subheader("Actions")
-    
-    # Display buttons for actions
-    if st.button("End Train"):
-        st.text("Game stopped.")
-
-    # You can remove the "Total Games Played" and "Highest Score" sections
-    # st.markdown("## Training Progress")
-    # st.text(f"Total Games Played: {agent.n_games}")
-    # st.text(f"Highest Score: {agent.highest_score}")
-
-# Add a divider for better separation
-st.markdown("---")
-
-# Display live chart in the main section
-st.subheader("Live Score Chart")
+if stop_btn:
+    st.text("Game stopped.")
