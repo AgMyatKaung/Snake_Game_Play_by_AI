aafebd8b6dd76c3ea4a2f1fd341c29cb7d0937c7
Aung Myat Kaung <107551174+AgMyatKaung@users.noreply.github.com>
2023-09-19 14:35:34 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,19 @@
 import streamlit as st
+import torch
 from game import SnakeGameAI, Direction, Point
 from agent import Agent  # Import your Agent class from agent.py
 import matplotlib.pyplot as plt
 import time
+import subprocess  # Import subprocess module
 
 # Function to run the game loop
-def run_game(agent, game, stop_flag):
+def run_game(agent, game):
     scores = []
     highest_score = 0
 
     chart = st.line_chart(scores)  # Initialize the live chart
 
-    while not stop_flag[0]:  # Use a flag to control the game loop
+    while True:
         state_old = agent.get_state(game)
         final_move = agent.get_action(state_old)
         reward, done, score, highest_score = game.play_step(final_move)
@@ -38,29 +40,48 @@ def run_game(agent, game, stop_flag):
         # Add a sleep to control the speed of the game (adjust as needed)
         time.sleep(0.01)  # Set a very low sleep duration to speed up the game
 
-# Create buttons for training and stopping the game
+# Set page title and description
+st.set_page_config(page_title="Snake Game AI", page_icon="🐍")
 st.title("Snake Game AI")
-st.text("Press 'Ctrl+C' to stop the game.")
-train_btn = st.button("Train AI")
-stop_btn = st.button("Stop")
 
-# Initialize the stop flag outside of the if block
-stop_flag = [False]
+# Create a two-column layout
+col1, col2 = st.columns([2, 1])
 
-if train_btn:
-    if not stop_flag[0]:  # Check if the game is not already running
+# Column 1: Snake Game AI
+with col1:
+    st.subheader("Game Display")
+    
+    # Create or load the agent and game instances
+    agent = Agent()
+    agent.load_model()
+    game = SnakeGameAI()
+
+    # Display game controls
+    st.markdown("## Game Controls")
+    st.text("Press 'Ctrl+C' to stop the game.")
+
+    # Check if "Train AI" button is clicked
+    if st.button("Train AI"):
         st.text("AI training in progress...")
+        
+        # Use subprocess to run the game in the console
+        subprocess.Popen(["python", "your_game_script.py"])
 
-        # Create or load the agent and game instances
-        agent = Agent()
-        agent.load_model()
-        game = SnakeGameAI()
+# Column 2: Buttons
+with col2:
+    st.subheader("Actions")
+    
+    # Display buttons for actions
+    if st.button("End Train"):
+        st.text("Game stopped.")
 
-        # Run the game loop
-        run_game(agent, game, stop_flag)
-    else:
-        st.text("Resuming AI training...")
+    # You can remove the "Total Games Played" and "Highest Score" sections
+    # st.markdown("## Training Progress")
+    # st.text(f"Total Games Played: {agent.n_games}")
+    # st.text(f"Highest Score: {agent.highest_score}")
 
-if stop_btn:
-    st.text("Stopping AI training...")
-    stop_flag[0] = True  # Set the flag to stop the game loop
+# Add a divider for better separation
+st.markdown("---")
+
+# Display live chart in the main section
+st.subheader("Live Score Chart")
